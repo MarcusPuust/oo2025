26c6963398e4ac8505ad44570c106d8edda7d434
Marcus Puust <marcusp@tlu.ee>
2025-03-21 15:29:40 +0200

    Front-end, 21.03 tund

--- 06_14.03_front-end/frontend/src/App.tsx
+++ 06_14.03_front-end/frontend/src/App.tsx
@@ -1,78 +1,56 @@
-import { useEffect, useState } from 'react'
+
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import { Category } from './models/Category';
-import { Product } from './models/Product';
+import { Route, Routes } from 'react-router-dom'
+import MainPage from './pages/MainPage';
+import ManageProducts from './pages/ManageProducts';
+import Arrayd from './pages/Arrayd';
+import Menu from './components/Menu';
+import ManageCategories from './pages/ManageCategories';
+import Cart from './pages/Cart';
+import Signup from './pages/Signup';
+import Orders from './pages/Orders';
+import Login from './pages/Login';
+
+
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const sonad = ["Elas", "metsas", "mutionu"];
-  const autod = [
-    {"mark": "BMW", "mudel": "i5", "year": 2015},
-    {"mark": "Audi", "mudel": "TT", "year": 2016},
-    {"mark": "Mercedes", "mudel": "S-class", "year": 2019},
-    {"mark": "VW", "mudel": "Golf", "year": 2012} //objekt
-  ];
-
-  //Muutuja - HTML muudab muutujat + HTMLi sulgude sees - algväärtus
-  const [kategooriad, setKategooriad] = useState<Category[]>([]);
-  const [products, setProducts] = useState<Product[]>([]);
-
-
-  //uef -> onload
-  useEffect(() => {
-    fetch("http://localhost:8080/categories") //api otspunkt kuhu läheb päring
-        .then(res=>res.json()) //kogu tagastus: headers, status code
-        .then(json=> setKategooriad(json)) // body: sisu, mida tagastab meile back-end
-    
-  }, []);
-
-  useEffect(() => {
-    fetch("http://localhost:8080/products") //api otspunkt kuhu läheb päring
-        .then(res=>res.json()) //kogu tagastus: headers, status code
-        .then(json=> setProducts(json)) // body: sisu, mida tagastab meile back-end
-    
-  }, []);
+  
   return (
     <>
-     {/* <div>{7 + 7}</div>
-     <div>7 + 7</div>
-     <div>{kogus}</div>
-     <div>{count}</div> */}
-     {sonad.map(sona => 
-      <div key={sona}>
-        {sona}
-      </div> )}
-     <br />
-     <br />
-     {/* <br /> Selfclosing break */}
-     {autod.map(auto =>
-       <div key={auto.mark+auto.mudel}>
-        {auto.mark} - {auto.mudel} ({auto.year})
-        </div> )}
-      <br />
-      <br />
-      {kategooriad.map(kategooria =>
-       <div key={kategooria.id}>
-        {kategooria.name} {kategooria.active}
-        </div> )}
-      <br />
-      <br />
-      {products.map(product =>
-       <div key={product.id}>
-        <div>{product.id}</div>
-        <div>{product.name}</div>
-        <div>{product.price}</div>
-        <div>{product.image}</div>
-        <div>{product.category?.name}</div>
-        {/* <div>{product.active}</div> kõik mis siin all on välja kuvamise eesmärgiga*/}
-        </div> )}
-    </>
-  )
+    
+     
+
+{/*  localhost:5173/ ---> <div>MainPage</div> */}
+{/* localhost:5173/admin/products ---> <div>ManageProducts</div> */}
+
+{/* Routidest üleval on menüü */}
+<Menu />
+
+<Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/admin/products" element={<ManageProducts />} />
+        <Route path="/admin/categories" element={<ManageCategories />} />
+
+        <Route path="/arrays" element={<Arrayd />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/arrays" element={<Arrayd />} />
+
+        <Route path="/*" element={<div> Page not found</div>} />
+
+      </Routes>
+      {/* Käib FOOTER */}
+      </>
+  );
 }
 
 // key={}
 // React soovib koodi mällu jätta. Kui toimuvad re-renderdused, siis ta jätab kõik mällu v.a. tsükli sisud, sest tal pole aimu, mille järgi seda meelde jätta. Selle jaoks, et ta saaks array meelde jätta, lisame key={}
+// tsükli/array sisud, sest tal pole mingit aimu, mille järgi seda meelde jätta.
+// selle jaoks, et ta saaks array meelde jätta, lisame key={}
 
 export default App
