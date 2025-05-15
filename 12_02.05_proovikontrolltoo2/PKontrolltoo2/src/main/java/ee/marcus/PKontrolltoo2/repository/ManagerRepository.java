6a2c2baa2e73a5059b1062c99acd42361e66605c
Marcus Puust <marcusp@tlu.ee>
2025-05-15 22:55:06 +0300

    Removed not working files

--- 12_02.05_proovikontrolltoo2/PKontrolltoo2/src/main/java/ee/marcus/PKontrolltoo2/repository/ManagerRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.marcus.PKontrolltoo2.repository;
-
-import ee.marcus.PKontrolltoo2.entity.Manager;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManagerRepository extends JpaRepository<Manager, Long> {
-}
