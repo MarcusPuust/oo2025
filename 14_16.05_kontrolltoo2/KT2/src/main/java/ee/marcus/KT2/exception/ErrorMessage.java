2ab6f74909bfc3863317a290650fbea5224e9d8c
Marcus Puust <marcusp@tlu.ee>
2025-05-15 20:22:48 +0300

    Kontrolltöö2 template backend + frontend

---
+++ 14_16.05_kontrolltoo2/KT2/src/main/java/ee/marcus/KT2/exception/ErrorMessage.java
@@ -0,0 +1,11 @@
+package ee.marcus.KT2.exception;
+
+import lombok.Data;
+import java.util.Date;
+@Data
+
+public class ErrorMessage {
+    private String message;
+    private Date timestamp;
+    private int status;
+}
